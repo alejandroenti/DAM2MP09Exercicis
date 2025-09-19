49daa7a0adf30f18b363e0391e4eada7c5e67ba2
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-09-19 16:10:43 +0200

    Exercici 0 fet

--- 00 Java - Processos/Exercici0/src/main/java/com/project/Main.java
+++ 00 Java - Processos/Exercici0/src/main/java/com/project/Main.java
@@ -1,28 +1,47 @@
 package com.project;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Informació que volem passar als threads
-        String info1 = "Missatge pel Thread 1A";
-        String info2 = "Missatge pel Thread 2A";
-        int number = 42;
+        
+        ExecutorService executor = Executors.newFixedThreadPool(2);
 
-        // Thread amb lambda que rep info1
-        new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " → " + info1);
-        }, "Thread 1").start();
-
-        // Thread amb lambda que rep info2 i un número
-        new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " → " + info2 + " i el número " + number);
-        }, "Thread 2").start();
-
-        // Thread amb classe anònima que també fa servir informació
-        new Thread() {
+        Runnable systemEvents = new Runnable() {
+            
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getName() + " → " + "Execució amb classe anònima, número *2 = " + (number * 2));
+                try {
+                    Thread.sleep(1000);
+                    System.out.println("Event del sistema enregistrat");
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
-        }.start();
+        };
+
+        Runnable networkStatus = new Runnable() {
+            
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    System.out.println("Tenim problemes amb la xarxa!");
+                    System.out.println("Sortint del sistema...");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        executor.execute(systemEvents);
+        executor.execute(networkStatus);
+
+        executor.shutdown();
     }
 }
