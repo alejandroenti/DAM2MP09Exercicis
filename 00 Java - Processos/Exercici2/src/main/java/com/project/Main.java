2ac55437b3380e4d591b88cf618e4fa96771117f
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-09-21 13:53:44 +0200

    Acabant Exercici 2

--- 00 Java - Processos/Exercici2/src/main/java/com/project/Main.java
+++ 00 Java - Processos/Exercici2/src/main/java/com/project/Main.java
@@ -11,36 +11,46 @@ public class Main {
 
         ExecutorService exec = Executors.newFixedThreadPool(2, r -> {
             Thread t = new Thread(r);
-            t.setDaemon(true);     // ajuda a no retenir el JVM
+            t.setDaemon(true); // ajuda a no retenir el JVM
             return t;
         });
 
         try {
-            CompletableFuture<HashMap<String, String>> recieveData =
-                CompletableFuture.supplyAsync(() -> {
-                    HashMap<String, String> map = new HashMap<String, String>();
-                    map.put("name", "Alejandro");
-                    map.put("email", "superemail@mail.co.uk");
-                    map.put("method", "POST");
-                    return map;
-                }, exec);
+            CompletableFuture<HashMap<String, String>> recieveData = CompletableFuture.supplyAsync(() -> {
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("name", "Alejandro");
+                map.put("email", "superemail@mail.co.uk");
+                map.put("method", "POST");
+                return map;
+            }, exec);
 
             CompletableFuture<String> processData = recieveData.thenApplyAsync(result -> {
-                String resultString = "";
+                if (result.get("name") != "Alejandro")
+                    return "Wrong name";
+                if (result.get("email") != "superemail@mail.co.uk")
+                    return "Wrong email";
+                if (result.get("method") != "POST")
+                    return "Wrong petion method";
 
-
-
-                return resultString;
+                return "OK";
             }, exec);
 
-            CompletableFuture<HashMap<Integer, String>> f3 = processData.thenApplyAsync(result -> {
-                return new HashMap<Integer, String>() {200, result};
+            CompletableFuture<String> sendResponse = processData.thenApplyAsync(result -> {
+                String response = "";
+
+                if (result == "OK") {
+                    response = String.format("{200: %s}", result);
+                } else {
+                    response = String.format("{401: %s}", result);
+                }
+
+                return response;
             }, exec);
 
-            Integer finalResult = f3.join();   // o get()
+            String finalResult = sendResponse.join(); // o get()
             System.out.println("Resultat final: " + finalResult);
         } finally {
-            exec.shutdown();                   // important!
+            exec.shutdown(); // important!
         }
     }
 }
