fa2132991ae48f698b5a6e1c182914913aa29189
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-12-15 18:20:28 +0100

    Eliminar comentaris de no implementat

--- 04 NodeJS - JavaScript/BuscaTresors/application/commands/CommandAjuda.js
+++ 04 NodeJS - JavaScript/BuscaTresors/application/commands/CommandAjuda.js
@@ -6,8 +6,8 @@ class CommandAjuda extends Command {
         menuTerminal.displayMessage(
             "Llista de comandes:\n" +
             " - ajuda|help - Mostra aquest menú\n" +
-            " - carregar partida 'nom_arxiu.json' - carrega una partida guardada (NO IMPLEMENTAT)\n" +
-            " - guardar partida 'nom_guardar.json' - guardar una partida amb el nom indicat (NO IMPLEMENTAT)\n" + 
+            " - carregar partida 'nom_arxiu.json' - carrega una partida guardada\n" +
+            " - guardar partida 'nom_guardar.json' - guardar una partida amb el nom indicat\n" + 
             " - activar trampes / desactivar trampes - es mostra/amaga un segon tauler amb les caselles destapades\n" +
             " - destapar L#, per exemple 'destapar B3' - desmarca la casella indica a la coordenada (Lletra/Fila, Número/Columna), i indica la distància a la que es troba el tresor més proper\n" +
             " - puntuacio - mostra la puntuació actual (tresors aconseguits) i les tirades restants\n" +
