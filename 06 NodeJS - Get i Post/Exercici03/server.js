bc2a2342accd39a96862d8608895fc104eb7bf33
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2026-03-16 17:33:03 +0100

    Removing comments

--- 06 NodeJS - Get i Post/Exercici03/server.js
+++ 06 NodeJS - Get i Post/Exercici03/server.js
@@ -6,10 +6,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
-// Carpeta pública para imágenes (Accesibles en /images/...)
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
-// Funciones para leer los archivos JSON privados
 const getCategories = () => {
     const data = fs.readFileSync(path.join(__dirname, 'data/categories.json'), 'utf8');
     return JSON.parse(data);
@@ -17,27 +15,23 @@ const getCategories = () => {
 
 const getItems = () => {
     const data = fs.readFileSync(path.join(__dirname, 'data/items.json'), 'utf8');
-    return JSON.parse(data).items; // Accedemos al array "items"
+    return JSON.parse(data).items; 
 };
 
-// 1. Demanar categories (POST)
 app.post('/categories', (req, res) => {
     const categories = getCategories();
-    // Retorna el objeto completo de categorías (Accio, Comedia, etc.)
     res.json(categories);
 });
 
-// 2. Demanar items d'una categoria (POST)
 app.post('/categoria/items', (req, res) => {
-    const { categoria } = req.body; // Ej: { "categoria": "Terror" }
+    const { categoria } = req.body;
     const allItems = getItems();
     const filtrats = allItems.filter(i => i.categoria === categoria);
     res.json({ items: filtrats });
 });
 
-// 3. Demanar informació d'un ítem (POST)
 app.post('/item/detall', (req, res) => {
-    const { id } = req.body; // Ej: { "id": 16 }
+    const { id } = req.body;
     const allItems = getItems();
     const item = allItems.find(i => i.id === parseInt(id));
     
@@ -48,9 +42,8 @@ app.post('/item/detall', (req, res) => {
     }
 });
 
-// 4. Demanar items d'una cerca (POST)
 app.post('/cerca', (req, res) => {
-    const { query } = req.body; // Ej: { "query": "Nolan" }
+    const { query } = req.body;
     const allItems = getItems();
     
     const resultats = allItems.filter(i => 
@@ -60,8 +53,6 @@ app.post('/cerca', (req, res) => {
     res.json({ resultats });
 });
 
-// 5. Retornar la imatge d'un ítem (GET)
-// Esta ruta permite obtener imágenes si no usas la ruta directa de static
 app.get('/foto/:nom', (req, res) => {
     const nomArxiu = req.params.nom;
     res.sendFile(path.join(__dirname, 'public/images', nomArxiu));
@@ -75,8 +66,7 @@ app.listen(PORT, () => {
 process.on('SIGTERM', shutDown);
 process.on('SIGINT', shutDown);
 function shutDown() {
-    // Executar aquí el codi previ al tancament de servidor
-    
+    // Executar aquí el codi previ al tancament de servidorW
     console.log('Received kill signal, shutting down gracefully');
     httpServer.close()
     process.exit(0);
